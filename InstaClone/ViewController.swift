ed59712499238d5a395417cfca23a9de3385a474
mitsuyoshi <m123matsuo@icloud.com>
2019-04-17 13:49:00 +0900

    Log in User Interface

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by mitsuyoshi matsuo on 2019/04/17.
+//  Copyright © 2019 mitsuyoshi matsuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
