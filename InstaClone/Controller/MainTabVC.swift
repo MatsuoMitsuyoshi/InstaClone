9c30b8b0d6939d840cdfbca03e7b37e8fb5aef77
mitsuyoshi <m123matsuo@icloud.com>
2019-04-18 15:23:31 +0900

    Log In Using Firebase and Construct Main User Interface

---
+++ InstaClone/Controller/MainTabVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabVC.swift
+//  InstaClone
+//
+//  Created by mitsuyoshi matsuo on 2019/04/18.
+//  Copyright © 2019 mitsuyoshi matsuo. All rights reserved.
+//
+
+import UIKit
+
+class MainTabVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstaClone/Controller/UploadPostVC.swift
@@ -0,0 +1,30 @@
+//
+//  UploadPostVC.swift
+//  InstaClone
+//
+//  Created by mitsuyoshi matsuo on 2019/04/18.
+//  Copyright © 2019 mitsuyoshi matsuo. All rights reserved.
+//
+
+import UIKit
+
+class UploadPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
