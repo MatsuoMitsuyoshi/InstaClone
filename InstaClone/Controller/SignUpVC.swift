2ad219f63eb6d3e823f0886ac612686057140fb7
mitsuyoshi <m123matsuo@icloud.com>
2019-04-18 11:53:04 +0900

    Creating User With Firebase/incomplete for Firebase version up

--- InstaClone/Controller/SignUpVC.swift
+++ InstaClone/Controller/SignUpVC.swift
@@ -9,13 +9,16 @@
 import UIKit
 import Firebase
 
-class SignUpVC: UIViewController {
+class SignUpVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: - Properties
-
+    
+    var imageSelected = false
+    
     let plusPhotoBtn: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleSelectProfilePhoto), for: .touchUpInside)
         return button
     }()
     
@@ -46,7 +49,7 @@ class SignUpVC: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
-//        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
         return tf
     }()
     
@@ -56,7 +59,7 @@ class SignUpVC: UIViewController {
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
         tf.font = UIFont.systemFont(ofSize: 14)
-//        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        tf.addTarget(self, action: #selector(formValidation), for: .editingChanged)
         return tf
     }()
     
@@ -81,7 +84,7 @@ class SignUpVC: UIViewController {
         
         return button
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -93,13 +96,49 @@ class SignUpVC: UIViewController {
         plusPhotoBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
         configureViewComponents()
-
+        
         view.addSubview(alreadyHaveAccountButton)
         alreadyHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
         
     }
-
+    
+    // MARK: - UIImagePickerController
+    
+    /// function that handles selecting image from camera roll
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // selected image
+        guard let profileImage = info[.editedImage] as? UIImage else {
+            imageSelected = false
+            return
+        }
+        
+        // set imageSelected to true
+        imageSelected = true
+        
+        // configure plusPhotoBtn with selected image
+        plusPhotoBtn.layer.cornerRadius = plusPhotoBtn.frame.width / 2
+        plusPhotoBtn.layer.masksToBounds = true
+        plusPhotoBtn.layer.borderColor = UIColor.black.cgColor
+        plusPhotoBtn.layer.borderWidth = 2
+        plusPhotoBtn.setImage(profileImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     // MARK: - Handlers
+
+    @objc func handleSelectProfilePhoto() {
+        
+        // configure image picker
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        // present image picker
+        self.present(imagePicker, animated: true, completion: nil)
+        
+    }
     
     // Back to SignUP
     @objc func handleShowLogin() {
@@ -107,29 +146,69 @@ class SignUpVC: UIViewController {
     }
     
     @objc func handleSignUp() {
-        
+
         // properties
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
-        
+        guard let fullName = fullNameTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
+
         Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
 
             if let error = error {
                 print("Failed to create user with error: ", error.localizedDescription)
                 return
             }
-            
-            print("Successfully created user with Firebase")
+
+            // set profile image
+            guard let profileImg = self.plusPhotoBtn.imageView?.image else { return }
+
+            // upload data
+            guard let uploadData = UIImageJPEGRepresentation(profileImg, 0.3) else { return }
+
+            // place image in firebase storage
+            let filename = NSUUID().uuidString
+            Storage.storage().reference().child("profile_images").putData(uploadData, metadata: nil, completion: {(metadata, error) in
+
+                // handle error
+                if let error = error {
+                    print("Failed to upload imgae to Firebase Storage with error", error.localizedDescription)
+                }
+
+                // profile image url
+                guard let profileImageURL = metadata?.downloadURL()?.absoluteString else { return }
+
+
+                //
+                guard let uid = user?.uid else { return }
+
+                let dictionaryValues = ["name": fullName,
+                                        "username": username,
+                                        "profileImageUrl": profileImageURL]
+
+                let values = [uid: dictionaryValues]
+
+                // save user info dagtabase
+                Database.database().reference().child("users").updateChildValues(dictionaryValues, withCompletionBlock: { (error, ref) in
+
+                    print("Successfuly created user and saved information to database")
+
+                })
+            })
         }
-            
-            
     }
+
+    
+    
     
     @objc func formValidation() {
         
         guard
             emailTextField.hasText,
-            passwordTextField.hasText else {
+            passwordTextField.hasText,
+            fullNameTextField.hasText,
+            usernameTextField.hasText,
+            imageSelected == true else {
                 signUpButton.isEnabled = false
                 signUpButton.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
                 return
@@ -150,5 +229,5 @@ class SignUpVC: UIViewController {
         view.addSubview(stackView)
         stackView.anchor(top: plusPhotoBtn.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 24, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 240)
     }
-
+    
 }
