c9521770c465d7cf971798899e51eee7c8ab5739
mitsuyoshi <m123matsuo@icloud.com>
2019-04-21 15:21:57 +0900

    User Search Cell

--- InstaClone/Controller/Search/SearchVC.swift
+++ InstaClone/Controller/Search/SearchVC.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuseIdentifier = "SearchUserCell"
 
 class SearchVC: UITableViewController {
+
+    // MARK: - Properties
+    
+    var users = [User]()
 
     // MARK: - Init
 
@@ -23,14 +28,18 @@ class SearchVC: UITableViewController {
         // separator insets
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 64, bottom: 0, right: 0)
         
+        // configure nav controller
         configurNavController()
+        
+        // fetch users
+        fetchUsers()
 
     }
 
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 64
+        return 60
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -38,11 +47,21 @@ class SearchVC: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return  users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let user = users[indexPath.row]
+        
+        print("Username is \(user.username)")
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SearchUserCell
+        
+        cell.user = users[indexPath.row]
+        
         return cell
     }
     
@@ -51,4 +70,29 @@ class SearchVC: UITableViewController {
     func configurNavController() {
         navigationItem.title = "Explore"
     }
+
+    // MARK: - API
+    
+    func fetchUsers() {
+        
+        Database.database().reference().child("users").observe(.childAdded) { (snapshot) in
+            
+            // uid
+            let uid = snapshot.key
+            
+            // snapshot value cast as dictionary
+            guard let dictionary = snapshot.value as? Dictionary<String, AnyObject> else { return }
+            
+            // construct user
+            let user = User(uid: uid, dictionary: dictionary)
+            
+            // append
+            self.users.append(user)
+            
+            // reload our table view
+            self.tableView.reloadData()
+            
+        }
+    }
 }
+
